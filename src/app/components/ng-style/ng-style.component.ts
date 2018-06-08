093d047bed9b932aa3fcad28f785fe2b5ede0ff9
Pablo Duran <pduranb90@gmail.com>
2018-06-08 02:55:50 -0400

    el uso de ng-style

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [style.fontSize.px]="tamano" >
+      Probando Ng-style
+    </p>
+
+    <p class="buttons">
+    <a class="button is-primary" (click)="tamano = tamano + 5">
+    <span class="icon"><i class="fa fa-plus"> </i></span> </a>
+
+    <a class="button is-danger" (click)="tamano = tamano - 5">
+    <span class="icon"><i class="fa fa-minus"> </i></span> </a>
+    </p>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano = 30;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
